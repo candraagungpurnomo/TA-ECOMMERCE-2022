91cffbb15cf308e289ec34120e729a30462bc725
candraagungpurnomo <122202002820@mhs.dinus.ac.id>
2022-06-27 15:47:22 +0700

    Project E-commerce Industri 2022

---
+++ ta-uts/README.md
@@ -0,0 +1,15 @@
+## 🚀 Pembuatan Project UAS
+
+Project UAS Ecommerce Joleboshop
+
+<hr>
+
+
+
+
+----------------------------------------->> HASIL <<-----------------------------------------
+
+- <b>Tampilan Project UAS</b> 
+
+![projectuas](https://user-images.githubusercontent.com/80149680/175893954-670eeb40-0fcf-44d5-81fa-9d548fa15c6b.png)
+
---
+++ ta-uts/joleboshop/application/views/user/_partials/footer.php
@@ -0,0 +1,17 @@
+<div class="bg-warning text-white py-5">
+
+    <div class="container row m-auto">
+
+        
+
+        <div class="col-12 text-center align-item-bottom mt-auto">
+
+            <small>Copyright &copy; 2022 &bull; JOLEBOSHOP</small><br>
+
+        Candra Agung Purnomo - A22.2020.02820
+
+        </div>
+
+    </div>
+
+</div>
